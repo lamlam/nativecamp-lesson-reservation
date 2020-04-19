44e40dd6938f7097f58c7c603f1e77232a0e2e3f
lamlam <lamlam4r@gmail.com>
2020-04-19 16:43:11 +0900

    Open native camp and click login button

---
+++ reserve.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+
+let screenshotId = 0;
+async function screenshotWithId(page, baseName = "screenshots/screenshot") {
+  const screenshotIdString = String(screenshotId).padStart(3, "0");
+  await page.screenshot({ path: `${baseName}${screenshotIdString}.png` });
+  screenshotId += 1;
+}
+
+(async () => {
+  // puppeteerを立ち上げます
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 1200,
+      height: 800,
+    },
+  });
+
+  try {
+    const page = await browser.newPage();
+    // pageを目的のURLへ移動します
+    await page.goto("https://nativecamp.net/waiting/detail/3306");
+    // スクリーンショットを取ります
+    await screenshotWithId(page);
+
+    // ログインをクリック
+    const [response] = await Promise.all([
+      page.waitForNavigation(),
+      page.click('a[href="https://nativecamp.net/login"]'),
+    ]);
+    // スクリーンショットを取ります
+    await screenshotWithId(page);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    if (browser) {
+      // puppeteerを終了します
+      await browser.close();
+    }
+  }
+})();
