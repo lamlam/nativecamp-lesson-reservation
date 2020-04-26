67354f6e0778a6435ab88e3ae4d430b35b7fadca
lamlam <lamlam4r@gmail.com>
2020-04-26 18:05:03 +0900

    Complete reservation handling

--- reserve.js
+++ reserve.js
@@ -1,11 +1,59 @@
 const puppeteer = require('puppeteer');
 
-const nativecampTopUrl = 'https://nativecamp.net/';
-const nativecampLoginUrl = 'https://nativecamp.net/login';
-
 const TEACHER_IDS = process.env.RESERVE_TEACHER_IDS;
 const EMAIL = process.env.NC_EMAIL;
 const PASSWORD = process.env.NC_PASSWORD;
+
+const nativecampTopUrl = 'https://nativecamp.net/';
+const nativecampLoginUrl = 'https://nativecamp.net/login';
+const nativecampMypageUrl = 'https://nativecamp.net/mypage';
+
+const DAY_OF_WEEK = {
+  sun: '日',
+  mon: '月',
+  tue: '火',
+  wed: '水',
+  thu: '木',
+  fri: '金',
+  sat: '土',
+};
+
+const RESERVABLE_BUTTON_SELECTER = '.btn_style.btn_green.forReservation';
+const DOUBLE_BOOKING_BUTTON_SELECTER = '.btn_style.double_booking';
+const ALREADY_RSERVED_BUTTON_SELECTER = '.btn_style.reserved.forCancellation';
+const RESERVE_MODAL_VISIBLE_SELECTER = '.title.t_truncate';
+const RESERVE_SELECT_COURSE_BUTTON_SELECTER =
+  '#chooseReservedSchedule.btn_style.btn_green.btnLessonOrReserve';
+const RESERVE_ALT_TEACHER_SWITCH_SELECTER = '.nc_ui_checkbox_switch_slider';
+const RESERVE_ALT_TEACHER_BUTTON_SELECTER =
+  '#chooseReservedSchedule.btn_style.btn_green.close_modal';
+const RESERVE_CONFIRM_MODAL_SELECTER = '#dialog_schedule_reserve_confirm';
+const RESERVE_CONFIRM_BUTTON =
+  '#saveReservedSchedule.btn_style.btn_green.close_modal.btnReservationConfirmed';
+const RESERVE_COMPLETE_MODAL_SELECTER = '#dialog_schedule_reserve_complete';
+const RESERVE_COMPLETE_MODAL_CLOSE_BUTTON_SELECTER =
+  '#dialog_schedule_reserve_complete > .btn_close.close_modal';
+
+const getPreferableTimeSlot = () => {
+  /*
+    example output
+    {
+      '月': ['08:00', '08:30', '21:00'],
+      '火': ['08:00', '08:30', '21:00'],
+    }
+  */
+  let preferableTimeSlot = {};
+  [
+    DAY_OF_WEEK.mon,
+    DAY_OF_WEEK.tue,
+    DAY_OF_WEEK.wed,
+    DAY_OF_WEEK.thu,
+    DAY_OF_WEEK.fri,
+  ].map((day) => {
+    preferableTimeSlot[day] = ['08:00', '08:30', '09:00'];
+  });
+  return preferableTimeSlot;
+};
 
 function getTeacherPageUrl(teacherID) {
   if (!teacherID) {
@@ -37,29 +85,201 @@ async function login(page, email, password) {
 
   await Promise.all([
     page.waitForNavigation(),
-    await page.click('button[type="submit"]'),
+    page.click('button[type="submit"]'),
   ]);
   await screenshotWithId(page);
   return true;
 }
 
-async function reserve(page, teacherID, isLoggedIn = false) {
+async function isLoggedIn(page) {
+  if (!page) {
+    throw new Error('page cannot be null or undefined.');
+  }
+
+  await page.goto(nativecampMypageUrl);
+
+  // if succeeded to login, location is mypage
+  // otherwise locaiton is login page
+  return page.url() === nativecampMypageUrl;
+}
+
+async function doReserve(page, reserveButton) {
+  let isError = false;
+  await Promise.all([
+    page.waitForSelector(RESERVE_MODAL_VISIBLE_SELECTER),
+    reserveButton.click(),
+  ]).catch((e) => {
+    console.log(e);
+    isError = true;
+  });
+  await screenshotWithId(page);
+  if (isError) {
+    return false;
+  }
+
+  const course = await page.$eval(
+    RESERVE_MODAL_VISIBLE_SELECTER,
+    (node) => node.innerText,
+  );
+  if (!course.includes('カランコース')) {
+    console.log('not selected カラン.');
+  }
+
+  const selectCourseButton = await page.$(
+    RESERVE_SELECT_COURSE_BUTTON_SELECTER,
+  );
+  await Promise.all([
+    page.waitForSelector(RESERVE_ALT_TEACHER_SWITCH_SELECTER),
+    selectCourseButton.click(),
+  ]).catch((e) => {
+    console.log(e);
+    isError = true;
+  });
+  await screenshotWithId(page);
+  if (isError) {
+    return false;
+  }
+
+  const switchAltTeacher = await page.$(RESERVE_ALT_TEACHER_SWITCH_SELECTER);
+  await switchAltTeacher.click();
+
+  const switchAltTeacherButton = await page.$(
+    RESERVE_ALT_TEACHER_BUTTON_SELECTER,
+  );
+  await Promise.all([
+    page.waitForSelector(RESERVE_CONFIRM_MODAL_SELECTER, { visible: true }),
+    switchAltTeacherButton.click(),
+  ]).catch((e) => {
+    console.log(e);
+    isError = true;
+  });
+  await screenshotWithId(page);
+  if (isError) {
+    return false;
+  }
+
+  const reserveConfirmButton = await page.$(RESERVE_CONFIRM_BUTTON);
+  await Promise.all([
+    page.waitForSelector(RESERVE_COMPLETE_MODAL_SELECTER, {
+      visible: true,
+    }),
+    reserveConfirmButton.click(),
+  ]).catch((e) => {
+    console.log(e);
+    isError = true;
+  });
+  await screenshotWithId(page);
+
+  const closeModalButton = await page.$(
+    RESERVE_COMPLETE_MODAL_CLOSE_BUTTON_SELECTER,
+  );
+  await Promise.all([
+    page.waitForSelector(RESERVE_COMPLETE_MODAL_SELECTER, {
+      hidden: true,
+    }),
+    closeModalButton.click(),
+  ]).catch((e) => {
+    console.log(e);
+    isError = true;
+  });
+  await screenshotWithId(page);
+
+  console.log('success reseravation');
+  return true;
+}
+
+async function findReservableTimeAndReserve(
+  page,
+  reservableAreaElement,
+  reservableTime,
+) {
+  if (!reservableAreaElement) {
+    throw new Error('reservableAreaElement cannot be null or undefined.');
+  }
+  if (!reservableTime || reservableTime.length === 0) {
+    throw new Error('reservableTime cannot be null or undefined.');
+  }
+
+  const day = await reservableAreaElement.$eval('.day', (node) =>
+    node.innerText.substring(1, 2),
+  );
+  if (!reservableTime.hasOwnProperty(day)) {
+    return false;
+  }
+
+  const doubleBookingElement = await reservableAreaElement.$(
+    DOUBLE_BOOKING_BUTTON_SELECTER,
+  );
+  if (doubleBookingElement) {
+    console.log('double booking found.', day);
+    return false;
+  }
+
+  const alreadyRservedElement = await reservableAreaElement.$(
+    ALREADY_RSERVED_BUTTON_SELECTER,
+  );
+  if (alreadyRservedElement) {
+    console.log('already reserved.', day);
+    return false;
+  }
+
+  const reserveButtons = await reservableAreaElement.$$(
+    RESERVABLE_BUTTON_SELECTER,
+  );
+  if (reserveButtons.length === 0) {
+    console.log('reservable time not found.', day);
+    return false;
+  }
+
+  let isReserved = false;
+  for (reserveButton of reserveButtons) {
+    if (isReserved) {
+      break;
+    }
+    try {
+      const timeOfButton = await page.evaluate(
+        (el) => el.innerText,
+        reserveButton,
+      );
+      if (reservableTime[day].indexOf(timeOfButton) === -1) {
+        continue;
+      }
+      console.log('Try reserve', timeOfButton, day);
+      isReserved = await doReserve(page, reserveButton);
+    } catch (e) {
+      console.log(e);
+      continue;
+    }
+  }
+  if (!isReserved) {
+    console.log('unmatched to preferable time slot.', day);
+  }
+  return isReserved;
+}
+
+async function reserve(page, teacherID) {
   if (!page) {
     throw new Error('page cannot be null or undefined.');
   }
   if (!teacherID) {
     throw new Error('teacherID cannot be null or undefined.');
   }
-  if (!isLoggedIn) {
+  if (!isLoggedIn(page)) {
     throw new Error('need login before start reservation');
   }
 
   await page.goto(getTeacherPageUrl(teacherID));
   await screenshotWithId(page);
 
-  const elements = await page.$$('tr[class="free_reservable_area"');
-  for (const element of elements) {
-    await screenshotWithId(element);
+  const reservableAreaElements = await page.$$(
+    'tr[class="free_reservable_area"]',
+  );
+  for (const reservableAreaElement of reservableAreaElements) {
+    isSucceded = await findReservableTimeAndReserve(
+      page,
+      reservableAreaElement,
+      getPreferableTimeSlot(),
+    );
   }
 }
 
@@ -70,6 +290,7 @@ async function reserve(page, teacherID, isLoggedIn = false) {
       width: 1200,
       height: 800,
     },
+    slowMo: 100,
   };
   if (process.env.GITHUB_ACTION) {
     // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
@@ -78,12 +299,11 @@ async function reserve(page, teacherID, isLoggedIn = false) {
   try {
     browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-
-    const isLoggedIn = await login(page, EMAIL, PASSWORD);
+    await login(page, EMAIL, PASSWORD);
 
     const teacherIDs = TEACHER_IDS.split(',');
     for (const teacherID of teacherIDs) {
-      await reserve(page, teacherID, isLoggedIn);
+      await reserve(page, teacherID);
     }
   } catch (e) {
     console.log(e);
